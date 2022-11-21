468d4a189c677fdeeeff32d2f911aa8b43484f59
Ahmed Sallam <109031729+AhmedSallam100@users.noreply.github.com>
2022-11-21 20:26:26 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -27,3 +27,43 @@ function scrollTop() {
   else scrollTop.classList.remove("show-scroll");
 }
 window.addEventListener("scroll", scrollTop);
+
+// Dark & Light Mode
+
+const themeButton = document.getElementById("theme-button");
+
+const darkTheme = "dark-theme";
+const iconTheme = "uil-sun";
+
+// const selectedTheme = localStorage.getItem("selected-theme");
+// const selectedIcon = localStorage.getItem("selected-icon");
+
+// const getCurrentTheme = () =>
+//   document.body.classList.contains("darkTheme") ? "dark" : "light";
+// const getCurrentIcon = () =>
+//   themeButton.classList.contains("iconTheme") ? "uil-moon" : "uil-sun";
+
+
+// if (selectedTheme) {
+//   document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+//     darkTheme
+//   );
+//   themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
+//     iconTheme
+//   );
+// }
+if (localStorage.getItem("dark")) {
+  document.body.classList.add(darkTheme)
+  themeButton.classList.add(iconTheme);
+}
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // localStorage.setItem("selected-theme", getCurrentTheme());
+  // localStorage.setItem("selected-icon", getCurrentIcon());
+  if (document.body.classList == "dark-theme") {
+    localStorage.setItem("dark", darkTheme)
+  }else {
+    localStorage.removeItem("dark")
+  }
+})
