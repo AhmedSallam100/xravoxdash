2db0636b4250b4dab7fa551d342a305138a969e3
Ahmed Sallam <109031729+AhmedSallam100@users.noreply.github.com>
2022-11-29 21:37:39 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -79,140 +79,140 @@ setInterval(() => {
 
 // Sign in
 
-let input = document.querySelectorAll(".in-text")
-let btn = document.querySelector(".bttn")
-let form = document.querySelectorAll(".form")
-let logOut = document.querySelector(".m-js")
-let pass = document.querySelector(".pass")
-let username = document.getElementById("username")
+// let input = document.querySelectorAll(".in-text")
+// let btn = document.querySelector(".bttn")
+// let form = document.querySelectorAll(".form")
+// let logOut = document.querySelector(".m-js")
+// let pass = document.querySelector(".pass")
+// let username = document.getElementById("username")
 
 
 
-if (localStorage.getItem("name")) {
-  document.querySelector("body .sub-menu h2").innerHTML = localStorage.getItem("name")
-  form.forEach((ele) => {
-    ele.style.display = "none"
-  })
-  content.classList.remove("fb-58")
-  sidebar.classList.remove("fb-58")
-} else {
-  content.classList.add("fb-58")
-  sidebar.classList.add("fb-58")
-}
-logOut.addEventListener("click", function () {
-  localStorage.removeItem("name")
-  form.forEach((ele) => {
-    ele.style.display = "block"
-  })
-  content.classList.add("fb-58")
-  sidebar.classList.add("fb-58")
-  document.querySelector("body.dark-theme .sub-menu h2").innerHTML = "User964"
-})
+// if (localStorage.getItem("name")) {
+//   document.querySelector("body .sub-menu h2").innerHTML = localStorage.getItem("name")
+//   form.forEach((ele) => {
+//     ele.style.display = "none"
+//   })
+//   content.classList.remove("fb-58")
+//   sidebar.classList.remove("fb-58")
+// } else {
+//   content.classList.add("fb-58")
+//   sidebar.classList.add("fb-58")
+// }
+// logOut.addEventListener("click", function () {
+//   localStorage.removeItem("name")
+//   form.forEach((ele) => {
+//     ele.style.display = "block"
+//   })
+//   content.classList.add("fb-58")
+//   sidebar.classList.add("fb-58")
+//   document.querySelector("body.dark-theme .sub-menu h2").innerHTML = "User964"
+// })
 
-let crAc = document.querySelectorAll(".crAc")
-let sign = document.querySelector(".sign")
-let log = document.querySelector(".log")
+// let crAc = document.querySelectorAll(".crAc")
+// let sign = document.querySelector(".sign")
+// let log = document.querySelector(".log")
 
-crAc.forEach((ele) => {
-  ele.addEventListener("click", () => {
-    sign.classList.toggle("op-0")
-    log.classList.toggle("op-0")
-  })
-})
+// crAc.forEach((ele) => {
+//   ele.addEventListener("click", () => {
+//     sign.classList.toggle("op-0")
+//     log.classList.toggle("op-0")
+//   })
+// })
 
-let singU = document.querySelector(".sign-u")
-let singP = document.querySelector(".sign-p")
-let singBtn = document.querySelector(".sign-b")
+// let singU = document.querySelector(".sign-u")
+// let singP = document.querySelector(".sign-p")
+// let singBtn = document.querySelector(".sign-b")
 
-singBtn.addEventListener("click", () => {
-  if (singU.value && singP.value) {
-    localStorage.setItem("sign-U", singU.value)
-    localStorage.setItem("sign-P", singP.value)
-    sign.classList.toggle("op-0")
-    log.classList.toggle("op-0")
-  }
-})
-btn.onclick = function () {
-  if (localStorage.getItem("sign-U") == username.value && localStorage.getItem("sign-P") == pass.value) {
-    window.localStorage.setItem("name", username.value)
-    document.querySelector("body.dark-theme .sub-menu h2").innerHTML = username.value
-    form.forEach((ele) => {
-      ele.remove()
-    })
-  }else if (localStorage.getItem("sign-U") !== username.value && localStorage.getItem("sign-P") !== pass.value) {
-    btn.classList.toggle("anmit")
+// singBtn.addEventListener("click", () => {
+//   if (singU.value && singP.value) {
+//     localStorage.setItem("sign-U", singU.value)
+//     localStorage.setItem("sign-P", singP.value)
+//     sign.classList.toggle("op-0")
+//     log.classList.toggle("op-0")
+//   }
+// })
+// btn.onclick = function () {
+//   if (localStorage.getItem("sign-U") == username.value && localStorage.getItem("sign-P") == pass.value) {
+//     window.localStorage.setItem("name", username.value)
+//     document.querySelector("body.dark-theme .sub-menu h2").innerHTML = username.value
+//     form.forEach((ele) => {
+//       ele.remove()
+//     })
+//   }else if (localStorage.getItem("sign-U") !== username.value && localStorage.getItem("sign-P") !== pass.value) {
+//     btn.classList.toggle("anmit")
 
-  }
-  else if (localStorage.getItem("sign-U") !== username.value ) {
-    btn.classList.toggle("anmit")
-  }else if (localStorage.getItem("sign-P") !== pass.value) {
-    btn.classList.toggle("anmit")
-  }
-}
+//   }
+//   else if (localStorage.getItem("sign-U") !== username.value ) {
+//     btn.classList.toggle("anmit")
+//   }else if (localStorage.getItem("sign-P") !== pass.value) {
+//     btn.classList.toggle("anmit")
+//   }
+// }
 
 
 // Show Password
 
-let myButton = document.querySelectorAll("#myBtnss"),
-    myInput = document.querySelectorAll("#my-passsss");
+// let myButton = document.querySelectorAll("#myBtnss"),
+//     myInput = document.querySelectorAll("#my-passsss");
 
-myButton.forEach((ele) => {
-  ele.onclick = function() {
-    if (this.classList == "uil uil-eye btn-shape") {
-      myInput.forEach((ele) => {
-        ele.setAttribute("type", "text");
-      })
-      this.classList = "uil uil-eye-slash btn-shape"
-    } else {
-      myInput.forEach((ele) => {
-        ele.setAttribute("type", "password");
-        this.classList = "uil uil-eye btn-shape";
-      })
-    }
-  }
-})
+// myButton.forEach((ele) => {
+//   ele.onclick = function() {
+//     if (this.classList == "uil uil-eye btn-shape") {
+//       myInput.forEach((ele) => {
+//         ele.setAttribute("type", "text");
+//       })
+//       this.classList = "uil uil-eye-slash btn-shape"
+//     } else {
+//       myInput.forEach((ele) => {
+//         ele.setAttribute("type", "password");
+//         this.classList = "uil uil-eye btn-shape";
+//       })
+//     }
+//   }
+// })
 
 
 // Choose Img
 
-if (localStorage.getItem("img")) {
-  document.querySelector("#display-img").style.backgroundImage = localStorage.getItem("img");
-}
+// if (localStorage.getItem("img")) {
+//   document.querySelector("#display-img").style.backgroundImage = localStorage.getItem("img");
+// }
 
-logOut.addEventListener("click", function () {
-  localStorage.setItem("img", `url(img/user.png)`)
-})
+// logOut.addEventListener("click", function () {
+//   localStorage.setItem("img", `url(img/user.png)`)
+// })
 
-const img_input = document.querySelector("#img-input")
-var upload_img = "";
+// const img_input = document.querySelector("#img-input")
+// var upload_img = "";
 
-img_input.addEventListener("change", function() {
-  const reader = new FileReader();
-  reader.addEventListener("load", () => {
-    upload_img = reader.result;
-    document.querySelector("#display-img").style.backgroundImage = `url(${upload_img})`;
-    window.localStorage.setItem("img", `url(${upload_img})`)
-  })
-  reader.readAsDataURL(this.files[0]);
-})
-let tabs = document.querySelectorAll(".tabs li");
-let tabsArray = Array.from(tabs);
-let divs = document.querySelectorAll(".content4 > div");
-let divsArray = Array.from(divs);
+// img_input.addEventListener("change", function() {
+//   const reader = new FileReader();
+//   reader.addEventListener("load", () => {
+//     upload_img = reader.result;
+//     document.querySelector("#display-img").style.backgroundImage = `url(${upload_img})`;
+//     window.localStorage.setItem("img", `url(${upload_img})`)
+//   })
+//   reader.readAsDataURL(this.files[0]);
+// })
+// let tabs = document.querySelectorAll(".tabs li");
+// let tabsArray = Array.from(tabs);
+// let divs = document.querySelectorAll(".content4 > div");
+// let divsArray = Array.from(divs);
 
-tabsArray.forEach((ele) => {
-  ele.addEventListener("click", function (e) {
-    tabsArray.forEach((ele) => {
-      ele.classList.remove("active");
-    });
-    e.currentTarget.classList.add("active");
-    divsArray.forEach((div) => {
-      div.style.display = "none";
-    });
-    // console.log(e.currentTarget.dataset.cont);
-    document.querySelector(e.currentTarget.dataset.cont).style.display = "block";
-  });
-});
+// tabsArray.forEach((ele) => {
+//   ele.addEventListener("click", function (e) {
+//     tabsArray.forEach((ele) => {
+//       ele.classList.remove("active");
+//     });
+//     e.currentTarget.classList.add("active");
+//     divsArray.forEach((div) => {
+//       div.style.display = "none";
+//     });
+//     // console.log(e.currentTarget.dataset.cont);
+//     document.querySelector(e.currentTarget.dataset.cont).style.display = "block";
+//   });
+// });
 
 // Show Loader
 
